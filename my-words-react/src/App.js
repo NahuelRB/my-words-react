aea8dd1b1fa220a75fd8dbc6f27a70256537f403
Nahuel Barbosa <nbarbosa@zeitune.local>
2025-05-02 09:16:23 -0300

    Primer commit

---
+++ my-words-react/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Boton from './Components/boton/Boton'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Practicando</h1>
+
+      <Boton/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-words-react/src/Components/boton/Boton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Boton.css';
+
+function Boton(){
+    return(
+        <button className='botones'> Probando el boton ahora </button>
+    );
+}
+
+export default Boton;
